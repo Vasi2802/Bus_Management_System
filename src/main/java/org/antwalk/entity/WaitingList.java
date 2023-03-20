82e4dac9ac9564a6a1d4df2e2f739ac7c5e40da3
Subhashree Mitra <subhashreem@trainee.nrifintech.com>
2023-03-20 13:26:35 +0530

    added

--- src/main/java/org/antwalk/entity/WaitingList.java
+++ src/main/java/org/antwalk/entity/WaitingList.java
@@ -6,6 +6,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -17,7 +18,7 @@ import javax.validation.constraints.NotNull;
 public class WaitingList {
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name="waiting_id")
 	private long wid;
 	
